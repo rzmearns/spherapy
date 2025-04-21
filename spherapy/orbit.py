fix: orbit fromListOfPositions fix: off by one error in TimeSpan.num_steps

spherapy/orbit.py
@@ -139,20 +139,20 @@ class Orbit(object):
 
 
 		elif gen_type == 'POS_LIST':
-			data = self._dfltDataDict()
+			data_dict = self._dfltDataDict()
 			
-			data['timespan'] = timespan
-			data['pos'] = args[1]
+			data_dict['timespan'] = timespan
+			data_dict['pos'] = args[1]
 			# Assume linear motion between each position at each timestep; 
 			# Then assume it stops at the last timestep.
-			vel = data['pos'][1:] - data['pos'][:-1]
-			data['vel'] = np.concatenate((vel, np.array([[0, 0, 0]])))
-			data['period'] = None
-			data['period_steps'] = 1
-			data['name'] = ['Sat from position list']
+			vel = data_dict['pos'][1:] - data_dict['pos'][:-1]
+			data_dict['vel'] = np.concatenate((vel, np.array([[0, 0, 0]])))
+			data_dict['period'] = None
+			data_dict['period_steps'] = 1
+			data_dict['name'] = ['Sat from position list']
 			# Note that this doesn't define a period.
 			logger.warning("Warning: When generating satellite orbit from list of positions, `period` and `period_steps` will not be defined.")			
-			data['gen_type'] = 'position list'
+			data_dict['gen_type'] = 'position list'
 
 		else: 
 			logger.error("Invalid orbit generation option {}. Valid options are TLE, FAKE_TLE, POS_LIST, and ANALYITCAL.".format(gen_type))
@@ -164,14 +164,14 @@ class Orbit(object):
 			# Timescale for sun position calculation should use TDB, not UTC
 			# The resultant difference is likely very small
 			ephem_sun = Ephem.from_body(Sun, astropyTime(timespan.asAstropy(scale='tdb')), attractor=Earth)
-			data['sun_pos'] = np.asarray(ephem_sun.rv()[0].to(astropy_units.km))
+			data_dict['sun_pos'] = np.asarray(ephem_sun.rv()[0].to(astropy_units.km))
 
 			logger.info('Creating ephemeris for Moon using timespan')
 			ephem_moon = Ephem.from_body(Moon, astropyTime(timespan.asAstropy(scale='tdb')), attractor=Earth)
-			data['moon_pos'] = np.asarray(ephem_moon.rv()[0].to(astropy_units.km))
-			data['eclipse'] = self._calcEclipse(data['pos'],data['sun_pos'])
+			data_dict['moon_pos'] = np.asarray(ephem_moon.rv()[0].to(astropy_units.km))
+			data_dict['eclipse'] = self._calcEclipse(data_dict['pos'],data_dict['sun_pos'])
 
-		self._attributiseDataDict(data)
+		self._attributiseDataDict(data_dict)
 
 	@classmethod	
 	def fromListOfPositions(cls, timespan, positions, astrobodies=True):

spherapy/timespan.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class TimeSpan(object):
+	num_steps : int
 
 	def __init__(self, t0, timestep='1S', timeperiod='10S', timezone='00:00'):
 		"""
@@ -27,7 +28,7 @@ class TimeSpan(object):
 		----------
 		t0 : {datetime.datetime}
 			datetime object defining the start of the TimeSpan
-		num_steps: int
+		num_steps : int
 			Number of timesteps
 		timestep : {str}, optional
 			String describing the time step of the time span. The string is constructed 
@@ -79,18 +80,19 @@ class TimeSpan(object):
 			raise ValueError("Invalid timeperiod value, too small: {}".format(timeperiod))
 
 		# Calculate step numbers and correct for non integer steps.
-		self.num_steps = int(np.floor(self.time_period / self.time_step))
-		if self.num_steps != self.time_period / self.time_step:
+		n_down = int(np.floor(self.time_period / self.time_step))
+		if n_down != self.time_period / self.time_step:
 			logger.info("Rounding to previous integer number of timesteps")
-			self.end = self.num_steps * self.time_step + self.start
+			self.end = n_down * self.time_step + self.start
 			self.time_period = self.end - self.start
 			
 			logger.info("Adjusting TimeSpan to {} -> {} with {} seconds timestep".
 						format(self.start, self.end, self.time_step.total_seconds()))
 
-			logger.info("TimeSpan has {} timesteps".format(self.num_steps))
 
 		self._timearr = np.arange(self.start, self.end+self.time_step, self.time_step).astype(dt.datetime)
+		self.num_steps = len(self._timearr)
+		logger.info("TimeSpan has {} timesteps".format(self.num_steps))
 		self._skyfield_timespan = load.timescale()
 
 		for ii in range(len(self._timearr)):