69e5d52fc6718739c548acc11e6ba04beef43495
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-07-23 18:16:17 +1000

    Removing unused exceptions

--- spherapy/util/exceptions.py
+++ spherapy/util/exceptions.py
@@ -1,21 +1,9 @@
-# Generic exceptions that apply throughout satplot.
-
-class InputException(Exception):
-	'''Input is not valid
-	'''
-	pass
+"""Generic exceptions that apply throughout satplot."""
 
 class OutOfRange(Exception):
-	'''The value is out of the acceptable range
-	'''
+	"""The value is out of the acceptable range."""
 	pass
 
 class DimensionError(Exception):
-	'''Dimension of an array is invalid
-	'''
-	pass
-	
-class GeometryError(Exception):
-	'''For when a user tries to do something non-Euclidean
-	'''
+	"""Dimension of an array is invalid."""
 	pass
