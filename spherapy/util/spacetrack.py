1342144a05146dd1ead33a15b00db5a7984db547
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-07-23 13:33:35 +1000

    refactoring TLEGetter.fetchLatest and TLEGetter.fetchAll

--- spherapy/util/spacetrack.py
+++ spherapy/util/spacetrack.py
@@ -1,4 +1,5 @@
 import pathlib
+import logging
 import spacetrack as sp
 import spherapy
 import datetime as dt
@@ -9,6 +10,9 @@ from progressbar import progressbar
 import json
 
 MAX_RETRIES=3
+TIMEOUT=60
+
+logger = logging.getLogger(__name__)
 
 class TLEGetter:
 	def __init__(self, sat_id_list:list[int], user:str=None, passwd:str=None):
@@ -54,78 +58,122 @@ class TLEGetter:
 	def checkTLEFileExists(self, sat_id:int) -> bool:
 		return getTLEFilePath(sat_id).exists()
 
-	def fetchAll(self, sat_id:int) -> str|None:
-		retries = 0
-		while retries < MAX_RETRIES:
+	def fetchAll(self, sat_id:int) -> int|None:
+		attempt_number = 0
+		while attempt_number < MAX_RETRIES:
 			try:
-				res_str = self.stc.tle(norad_cat_id=sat_id, orderby='epoch asc', limit=500000, format='3le')
-				with open(getTLEFilePath(sat_id), 'w') as fp:
-					if res_str[-1] == '\n':
-						res_str = res_str[:-1]
-					fp.write(res_str)
+				opts = self._calcRequestAllOptions(sat_id)
+				resp_block = self.stc.tle(*opts)
+				self._writeResponseToNewFile(sat_id, resp_block)
 				break
-			except TimeoutError as e:
-				retries += 1
-		if retries == MAX_RETRIES:
-			print(f"Could not fetch All TLEs for sat {sat_id}: failed {retries} times.", file=sys.stderr)
+			except TimeoutError:
+				attempt_number += 1
+
+		if attempt_number == MAX_RETRIES:
+			logger.error("Could not fetch All TLEs for sat %s: failed %s times.",sat_id, attempt_number)
 			return None
 		
 		return sat_id
-		
 
 	def getNumPastTLEs(self, sat_id:int) -> int:
 		with open(getTLEFilePath(sat_id), 'r') as fp:
 			lines = fp.readlines()
 		return int(len(lines)/3)
 
-	def fetchLatest(self, sat_id:int) -> str|None:
-		retries = 0
-		while retries < MAX_RETRIES:
+	def fetchLatest(self, sat_id:int) -> int|None:
+		attempt_number = 0
+		while attempt_number < MAX_RETRIES:
 			try:
-				# get penultimate and ultimate epochs
-				with open(getTLEFilePath(sat_id), 'r') as fp:
-					lines = fp.readlines()
-				while lines[-1] == '':
-					lines = lines[:-1]
-				le_line = lines[-2]
-				# pe_line = lines[-5]
-				# pe_datetime = epoch_u.epoch2datetime(float(pe_line.split()[3]))
-				try:
-					le = float(le_line.split()[3])
-				except IndexError:
-					print(le_line)
-					raise IndexError
-				le_datetime = epoch_u.epoch2datetime(le)
-				delta = dt.datetime.now(tz=dt.timezone.utc) - le_datetime
-				if delta.days != 0:
-					res_str = self.stc.tle(norad_cat_id=sat_id, orderby='epoch asc', epoch=f'>now-{delta.days+1}', limit=500000, format='3le')
-					res_str = res_str[:-1]
-					res_lines = res_str.split('\n')
-					for ii, line in enumerate(res_lines):
-						# Try block for debugging, only sometimes failing, trying to investigate.
-						try:
-							if line[0] == '1' and float(line.split()[3])>le:
-								break
-						except IndexError:
-							print(f'{le=}')
-							print(f'{ii=}')
-							print(f'{line=}')
-							print(f'{line.split()}')
-							print(f'{res_lines}')
-					next_index = ii
-					if res_lines[0] != '':
-						with open(getTLEFilePath(sat_id), 'a') as fp:
-							fp.write('\n')
-							fp.write('\n'.join(res_lines[next_index-1:]))
+				_, days_since_last_epoch = self._findLocalLastEpoch(sat_id)
+				if days_since_last_epoch > 0.0:
+					opts = self._calcRequestPartialOptions(sat_id)
+					resp_block = self.stc.tle(*opts)
+					self._writeResponseToFile(sat_id, resp_block)
 				break
 			except TimeoutError as e:
 				print(e)
-				retries += 1
-		if retries == MAX_RETRIES:
-			print(f"Could not fetch the latest TLEs for sat {sat_id}: failed {retries} times.", file=sys.stderr)
+				attempt_number += 1
+		if attempt_number == MAX_RETRIES:
+			print(f"Could not fetch the latest TLEs for sat {sat_id}: failed {attempt_number} times.", file=sys.stderr)
 			return None
 
 		return sat_id
+
+	def _findLocalLastEpoch(self, sat_id:int) -> tuple[float, float]:
+		"""Find the last TLE epoch in a TLE file"""
+		# get penultimate and ultimate epochs
+		with getTLEFilePath(sat_id).open('r') as fp:
+			lines = fp.readlines()
+		while lines[-1] == '':
+			lines = lines[:-1]
+		last_epoch_line = lines[-2]
+		# pe_line = lines[-5]
+		# pe_datetime = epoch_u.epoch2datetime(float(pe_line.split()[3]))
+		try:
+			last_epoch = float(last_epoch_line.split()[3])
+		except IndexError:
+			print(last_epoch_line)
+			raise IndexError
+
+		last_epoch_datetime = epoch_u.epoch2datetime(last_epoch)
+		delta = dt.datetime.now(tz=dt.timezone.utc) - last_epoch_datetime
+
+		return last_epoch, delta.days
+
+	def _calcRequestAllOptions(self, sat_id:int) -> dict[str, str|int]:
+		"""Format spacetrack library request options. Request all TLEs"""
+
+		request_options = {}
+		request_options['norad_cat_id'] = sat_id
+		request_options['order_by'] = 'epoch_asc'
+		request_options['limit'] = 500000
+		request_options['format'] = '3le'
+
+		return request_options
+
+	def _calcRequestPartialOptions(self, sat_id:int) -> dict[str, str|int]:
+		"""Format spacetrack library request options. Request all TLEs since last epoch"""
+		_, days_since_last_epoch = self._findLocalLastEpoch(sat_id)
+
+		request_options = {}
+		request_options['norad_cat_id'] = sat_id
+		request_options['order_by'] = 'epoch_asc'
+		request_options['epoch'] = f'>now-{days_since_last_epoch+1}'
+		request_options['limit'] = 500000
+		request_options['format'] = '3le'
+
+		return request_options
+
+	def _writeResponseToFile(self, sat_id:int, resp:str):
+		"""Append response to TLE file"""
+		last_epoch, _ = self._findLocalLastEpoch(sat_id)
+		resp = resp[:-1]
+		resp_lines = resp.split('\n')
+		next_idx = 0
+		for ii, line in enumerate(resp_lines):
+			# Try block for debugging, only sometimes failing, trying to investigate.
+			try:
+				next_idx = ii
+				if line[0] == '1' and float(line.split()[3])>last_epoch:
+					break
+			except IndexError:
+				print(f'{last_epoch=}')
+				print(f'{ii=}')
+				print(f'{line=}')
+				print(f'{line.split()}')
+				print(f'{resp_lines}')
+
+		if resp_lines[0] != '':
+			with open(getTLEFilePath(sat_id), 'a') as fp:
+				fp.write('\n')
+				fp.write('\n'.join(resp_lines[next_idx-1:]))
+
+	def _writeResponseToNewFile(self, sat_id:int, resp:str):
+		"""New TLE, write entire content to new file"""
+		with open(getTLEFilePath(sat_id), 'w') as fp:
+			if resp[-1] == '\n':
+				resp = resp[:-1]
+			fp.write(resp)
 
 class InvalidCredentials(Exception):
 	def __init__(self, message):
