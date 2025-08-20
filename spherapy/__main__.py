2b2837c6e07c566505a9595ce90f2fc867d982c6
Robert Mearns <robert.mearns@unimelb.edu.au>
2025-08-20 21:29:06 +1000

    adding command line tool to clear credentials

--- spherapy/__main__.py
+++ spherapy/__main__.py
@@ -11,3 +11,7 @@ from spherapy import credentials
 def create_credentials():
 	"""Helper script to call create credentials function."""
 	credentials.createCredentials()
+
+def clear_credentials():
+	"""Helper script to call clear credentials function."""
+	credentials.clearCredentials()
